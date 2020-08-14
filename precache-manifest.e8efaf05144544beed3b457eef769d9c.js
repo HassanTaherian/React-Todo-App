d41693400f5673d0d08d9da9af69d4764bb246d1
HassanTSF <hassantsf.dev@gmail.com>
2020-08-14 23:44:40 +0430

    deploy

---
+++ precache-manifest.e8efaf05144544beed3b457eef769d9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b8c500f402ec45f95b751262e352da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f98fcd560e697235e2",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "de7317752b406ccd8208",
+    "url": "/static/css/main.18c36c3a.chunk.css"
+  },
+  {
+    "revision": "20f98fcd560e697235e2",
+    "url": "/static/js/2.d8dd4d9f.chunk.js"
+  },
+  {
+    "revision": "ee3dc16e82e8f5d54e0f02eca6b3ea91",
+    "url": "/static/js/2.d8dd4d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7317752b406ccd8208",
+    "url": "/static/js/main.09bfe115.chunk.js"
+  },
+  {
+    "revision": "b278acee63071185f4c1",
+    "url": "/static/js/runtime-main.ba9668b7.js"
+  }
+]);
